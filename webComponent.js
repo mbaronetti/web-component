0051bb998edd44b2609db5074d2477c85a42f4cf
mbaronetti <manuelbaronetti@Manuels-MacBook-Pro.local>
2020-10-15 19:30:00 +0200

    add files

---
+++ webComponent.js
@@ -0,0 +1,24 @@
+
+const template = document.createElement('template');
+template.innerHTML = `
+<div class="form-field">
+    <label>Email:</label>
+    <input type="email" />
+</div>
+<div class="form-field">
+    <label>Password:</label>
+    <input type="password" />
+</div>
+<slot name="informationText">
+    <p>This is a default piece of information text</p>
+</slot>`;
+
+class SignupForm extends HTMLElement {
+    constructor() {
+        super();
+        const shadowRoot = this.attachShadow({ mode: 'open' });
+        shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+}
+
+customElements.define('signup-form', SignupForm);
