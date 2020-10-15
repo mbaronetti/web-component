831e966e247e973c38084f6d4a46b5da39f2eba1
mbaronetti <manuelbaronetti@Manuels-MacBook-Pro.local>
2020-10-15 20:11:08 +0200

    polyfill

--- webComponent.js
+++ webComponent.js
@@ -1,30 +1,59 @@
+"use strict";
+
+function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return !!right[Symbol.hasInstance](left); } else { return left instanceof right; } }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _wrapNativeSuper(Class) { var _cache = typeof Map === "function" ? new Map() : undefined; _wrapNativeSuper = function _wrapNativeSuper(Class) { if (Class === null || !_isNativeFunction(Class)) return Class; if (typeof Class !== "function") { throw new TypeError("Super expression must either be null or a function"); } if (typeof _cache !== "undefined") { if (_cache.has(Class)) return _cache.get(Class); _cache.set(Class, Wrapper); } function Wrapper() { return _construct(Class, arguments, _getPrototypeOf(this).constructor); } Wrapper.prototype = Object.create(Class.prototype, { constructor: { value: Wrapper, enumerable: false, writable: true, configurable: true } }); return _setPrototypeOf(Wrapper, Class); }; return _wrapNativeSuper(Class); }
+
+function _construct(Parent, args, Class) { if (_isNativeReflectConstruct()) { _construct = Reflect.construct; } else { _construct = function _construct(Parent, args, Class) { var a = [null]; a.push.apply(a, args); var Constructor = Function.bind.apply(Parent, a); var instance = new Constructor(); if (Class) _setPrototypeOf(instance, Class.prototype); return instance; }; } return _construct.apply(null, arguments); }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _isNativeFunction(fn) { return Function.toString.call(fn).indexOf("[native code]") !== -1; }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
 window.supportsCustomElements = 'customElements' in window;
 window.supportsShadowDOM = Boolean(HTMLElement.prototype.attachShadow);
-
 console.log('supports custom elements: ', window.supportsCustomElements);
 console.log('supports shadowDOM: ', window.supportsShadowDOM);
+var template = document.createElement('template');
+template.innerHTML = "\n<div class=\"form-field\">\n    <label>Email:</label>\n    <input type=\"email\" />\n</div>\n<div class=\"form-field\">\n    <label>Password:</label>\n    <input type=\"password\" />\n</div>\n<slot name=\"informationText\">\n    <p>This is a default piece of information text</p>\n</slot>";
 
-const template = document.createElement('template');
-template.innerHTML = `
-<div class="form-field">
-    <label>Email:</label>
-    <input type="email" />
-</div>
-<div class="form-field">
-    <label>Password:</label>
-    <input type="password" />
-</div>
-<slot name="informationText">
-    <p>This is a default piece of information text</p>
-</slot>`;
+var SignupForm = /*#__PURE__*/function (_HTMLElement) {
+  _inherits(SignupForm, _HTMLElement);
 
-class SignupForm extends HTMLElement {
-    constructor() {
-        super();
-        const shadowRoot = this.attachShadow({ mode: 'open' });
-        shadowRoot.appendChild(template.content.cloneNode(true));
-    }
-}
+  var _super = _createSuper(SignupForm);
+
+  function SignupForm() {
+    var _this;
+
+    _classCallCheck(this, SignupForm);
+
+    _this = _super.call(this);
+
+    var shadowRoot = _this.attachShadow({
+      mode: 'open'
+    });
+
+    shadowRoot.appendChild(template.content.cloneNode(true));
+    return _this;
+  }
+
+  return SignupForm;
+}( /*#__PURE__*/_wrapNativeSuper(HTMLElement));
 
 customElements.define('signup-form', SignupForm);
-
